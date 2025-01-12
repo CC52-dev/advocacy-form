dea499d1021f25367d9e32c8d56b980cfbb27a9a
Eshwar B.Y <117953628+CC52-dev@users.noreply.github.com>
2025-01-12 11:26:40 -0600

    removed dist

--- backend/dist/src/db/index.js
+++
@@ -1,4 +0,0 @@
-import 'dotenv/config';
-import { drizzle } from 'drizzle-orm/node-postgres';
-// biome-ignore lint/style/noNonNullAssertion: <explanation>
-const db = drizzle(process.env.DATABASE_URL);
