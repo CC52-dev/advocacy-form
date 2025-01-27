786e94c25849764ad69e796fd55f5429ddf35c0c
Eshwar B.Y <117953628+CC52-dev@users.noreply.github.com>
2025-01-26 22:20:40 -0600

    feat: better looking landing page

--- frontend/src/app/page.jsx
+++ frontend/src/app/page.jsx
@@ -11,7 +11,7 @@ export default function Home() {
   return (
     <>
       <Nav />
-      <section className="flex min-h-screen items-center justify-center space-y-6 pb-8 pt-6 md:pb-12 md:pt-10 lg:py-32">
+      <section className="flex min-h-screen items-center justify-center space-y-6 pb-8 pt-6  md:pb-12 md:pt-10 lg:py-32 bg-gradient-to-b from-gray-100 to-white">
         <div className="container flex max-w-7xl flex-col items-center gap-4 text-center px-5">
           <Link
             href="https://www.facebook.com/satsankalpafoundation/"
@@ -20,8 +20,8 @@ export default function Home() {
           >
             Follow along on Facebook
           </Link>
-          <h1 className="font-heading text-3xl sm:text-5xl md:text-6xl lg:text-7xl">
-Sign Up For Satsankalpa Advocacy          </h1>
+          <h1 className="font-bold text-3xl sm:text-5xl md:text-6xl lg:text-7xl leading-tight tracking-tight text-gray-900">
+  Satsankalpa Advocacy          </h1>
           <p className="max-w-[42rem] leading-normal text-muted-foreground sm:text-xl sm:leading-8">
           Advocacy membership program's objective is to build a strong network of global supporters to further both the Foundations' vision, mission and activities. 
           </p>
