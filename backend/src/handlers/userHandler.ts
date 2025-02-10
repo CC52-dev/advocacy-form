aeef2844d58dca71dc6ea10386b2335b715b6ff8
Eshwar B.Y <117953628+CC52-dev@users.noreply.github.com>
2025-02-09 18:35:59 -0600

    feat: user backend

--- backend/src/handlers/userHandler.ts
+++ backend/src/handlers/userHandler.ts
@@ -26,3 +26,36 @@ export async function getUser(token: string, res: Response) {
     });
   }
 }
+
+export async function getAllUsers(token: string, res: Response) {
+  try {
+    const sessionValidationResult: SessionValidationResult =
+      await validateSessionToken(token);
+    if (
+      !sessionValidationResult.session ||
+      !sessionValidationResult.user ||
+      sessionValidationResult.user["type"] !== "admin"
+    ) {
+      res.status(400).json({ message: "Token is Invalid Or Expired" });
+      console.log({ message: "Token is Invalid Or Expired" });
+      return;
+    }
+    console.log({ message: sessionValidationResult.user });
+    const applicants = await db
+      .select()
+      .from(usersTable)
+      .where(eq(usersTable.type, "user"));
+    if (applicants) {
+      res.status(200).json({
+        message: applicants,
+      });
+      return;
+    }
+    res.status(200).json({
+      message: [],
+    });
+  } catch (error) {
+    res.status(400).json({ message: "An error occurred" });
+    console.error(error);
+  }
+}
--- backend/src/routes/user.ts
+++ backend/src/routes/user.ts
@@ -2,11 +2,16 @@ import express from "express";
 import type { Response, Request, NextFunction } from "express";
 const router = express.Router();
 import "dotenv/config";
-import { getUser } from "../handlers/userHandler.js";
+import { getUser, getAllUsers } from "../handlers/userHandler.js";
 
 router.post("/getUser", async (req: Request, res: Response) => {
   const token = req.headers.cookie?.split('session_token=')[1]?.split(';')[0];
   await getUser(token, res);;
 
+})
+
+router.post("/getAllUsers", async (req: Request, res: Response) => {
+  const token = req.headers.cookie?.split('session_token=')[1]?.split(';')[0];
+  await getAllUsers(token, res);
 })
 export default router;
