0d9a2d99b2fcd4bc1d23e4a310c2187099ef7d67
Eshwar B.Y <117953628+CC52-dev@users.noreply.github.com>
2025-03-19 21:16:38 -0500

    feat: better running

--- runbuild.js
+++ runbuild.js
@@ -2,12 +2,12 @@ const concurrently = require('concurrently');
 
 concurrently([
   { 
-    command: 'cd frontend && npm run build',
+    command: 'cd frontend && npm run build && npm run start',
     name: 'frontend',
     prefixColor: 'blue'
   },
   {
-    command: 'cd backend && npm run build',
+    command: 'cd backend && npm run build && npm run start',
     name: 'backend',
     prefixColor: 'green'
   }
