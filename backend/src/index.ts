cdd533e62aae2232af838ce9d8674d125a710a83
Eshwar B.Y <117953628+CC52-dev@users.noreply.github.com>
2025-05-11 11:28:13 -0500

    abc

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,6 +1,6 @@
 import app from "./server.js";
-const port = process.env.PORT || 80;
+const port = process.env.PORT || 3001;
 
-app.listen(80,'0.0.0.0', () => {
+app.listen(port, () => {
   console.log(`Server is listening at port ${port}`);
 });
