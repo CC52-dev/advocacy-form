70f69d108c7219cf133c4c8bc23c07078b2ec84b
Eshwar B.Y <117953628+CC52-dev@users.noreply.github.com>
2025-05-29 21:07:28 -0500

    fix: api Error

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:3001',
+  baseURL: 'https://services.satsankalpa.org',
   withCredentials: true, // This is important for handling cookies
 });
 
