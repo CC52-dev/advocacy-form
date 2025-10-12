e9bb7220f532c93b0c459e8c587d38e7a84df90c
Eshwar B.Y <eshwarb.y@outlook.com>
2025-10-12 18:24:33 -0500

    added connection pool

--- backend/src/db/db.ts
+++ backend/src/db/db.ts
@@ -5,22 +5,25 @@ import { URL } from "url";
 
 
 
-async function createConnection() {
+async function createPool() {
   try {
-    const connection = await mysql.createConnection({
-   host: process.env.DB_HOST,
-    port: parseInt(process.env.DB_PORT),
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
+    const pool = mysql.createPool({
+      host: process.env.DB_HOST,
+      port: parseInt(process.env.DB_PORT),
+      user: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      waitForConnections: true,
+      connectionLimit: 10,
+      queueLimit: 0
     });
-    return drizzle(connection);
+    return drizzle(pool);
   } catch (error) {
-    console.error("Failed to create database connection:", error);
+    console.error("Failed to create database pool:", error);
     throw error;
   }
 }
 
-const db = await createConnection();
+const db = await createPool();
 
 export default db;
